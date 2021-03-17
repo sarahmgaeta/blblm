c42bba1bb2c3c0687b6307e741f37893ac528383
sarahmgaeta <sarahgaeta.cc@gmail.com>
2021-03-16 23:42:18 -0700

    updating tests and vignettes

--- tests/testthat/test-confint.R
+++ tests/testthat/test-confint.R
@@ -1,4 +1,5 @@
 test_that("confint output is as expected", {
+  # Test confint works on blblm objects
   fit <- blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 100)
 
   # Expect 4 values: 2 lower bounds and 2 upper bounds
@@ -6,4 +7,13 @@ test_that("confint output is as expected", {
 
   # Expect outputs to be numeric
   expect_equal(class(confint(fit, c("wt", "hp"))[[1]]), "numeric")
+
+  # Test confint works on blblogreg objects
+  fit2 <- blblogreg(am ~ mpg, data = mtcars, m = 3, B = 100)
+
+  # Expect 2 values: 1 lower bounds and 1 upper bounds
+  expect_equal(length(confint(fit2, c("mpg"))), 2)
+
+  # Expect outputs to be numeric
+  expect_equal(class(confint(fit2, c("mpg"))[[1]]), "numeric")
 })
--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -1,4 +1,6 @@
 test_that("predict output is as expected", {
+
+  # Test predict works on blblm objects
   fit <- blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 100)
 
   new_values1 <- predict(fit, data.frame(wt = c(2.5, 3), hp = c(150, 170)))
@@ -16,4 +18,22 @@ test_that("predict output is as expected", {
   # Expect all output to be numeric
   expect_equal(class(new_values1), "numeric")
   expect_equal(class(new_values2[[1]]), "numeric")
+
+  # Test predict works on blblogreg objects
+  fit2 <- blblogreg(am ~ mpg, data = mtcars, m = 3, B = 100)
+
+  new_values12 <- predict(fit2, data.frame(mpg = 15.3, 17.8))
+  new_values22 <- predict(fit2, data.frame(mpg = 15.3, 17.8), confidence = TRUE)
+
+  # Expect the estimated new values to be the same regardless if confidence = TRUE or FALSE
+  expect_equal(new_values12[[1]], new_values22[[1]])
+
+  # Expect only 2 outputted estimates if confidence = FALSE
+  expect_equal(length(new_values12), 1)
+  # Expect 2 outputted estimates and their lower and upper bounds (6 total estimates) if confidence = TRUE
+  expect_equal(length(new_values22), 3)
+
+  # Expect all output to be numeric
+  expect_equal(class(new_values12), "numeric")
+  expect_equal(class(new_values22[[1]]), "numeric")
 })
